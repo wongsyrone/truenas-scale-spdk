03a44e0c3664c734410d2aac26e5a805ff7162d5
Brian M <brian.meagher@ixsystems.com>
2025-05-30 08:11:31 -0700

    Checkout using branch v24.09.x rather than a tag

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -ex
-TAG=v24.09
-
-# Supplying tag and depth on this line resulted in the wrong
-# submodules being checked out.  Avoid.
-git clone https://github.com/spdk/spdk --recursive spdk.src
-( cd spdk.src ; git checkout $TAG )
+BRANCH=v24.09.x
+git clone -b $BRANCH https://github.com/spdk/spdk --recursive spdk.src
 rsync -rv --exclude=.git spdk.src/* .
 rm -rf spdk.src fetch.sh
